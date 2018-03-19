9c31315035976eba68d8e2ff09c5022a9b70a1a3
Steef <stefanodegenkamp@gmail.com>
2018-03-19 17:36:14 +0100

    Relationships added to models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,3 @@
 class Listing < ApplicationRecord
+  has_many :tickets
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
   belongs_to :listing
-  belongs_to :user
+  belongs_to :user # only if the ticket is bought
+  has_many :barcodes # ticket can have more than one barcode
 end
