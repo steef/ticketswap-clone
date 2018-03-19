e3f22a2a2e91d8653a0d316ca3f68a4091be0eba
Steef <stefanodegenkamp@gmail.com>
2018-03-19 17:39:39 +0100

    Bought by user on the ticket table is now optional

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ApplicationRecord
   belongs_to :listing
-  belongs_to :user # only if the ticket is bought
+  belongs_to :user, optional: true # only if the ticket is bought
   has_many :barcodes # ticket can have more than one barcode
 end
