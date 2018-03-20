633fc3f3733d6453ceabb5ea0600ce7f48c39f5b
Steef <stefanodegenkamp@gmail.com>
2018-03-20 20:43:00 +0100

    Tickets controller build

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,10 +1,29 @@
 class TicketsController < ApplicationController
   def new
+    @listing = Listing.find(params[:listing_id])
+    @ticket = Ticket.new
   end
 
   def create
+    @listing = Listing.find(params[:listing_id])
+    @ticket = Ticket.new(ticket_params)
+    @ticket.listing = @listing
+    if @ticket.save
+      redirect_to @ticket.listing
+    else
+      render :new
+    end
   end
 
   def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:listing_id, :bought_by_user_id, :bought_at_date)
   end
 end
