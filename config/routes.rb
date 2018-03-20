53ff6a880c61a1bd5094ac6ad9bfd79e6a5f3e01
Steef <stefanodegenkamp@gmail.com>
2018-03-20 16:00:46 +0100

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :listings do
+    resources :tickets do
+    end
+  end
+
+  resources :tickets, only: [] do # barcodes are nested inside tickets
+    resources :barcodes do
+    end
+  end
 end
