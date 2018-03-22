7c92eadce3d98d2d26989d53eaae88132e726565
Steef <stefanodegenkamp@gmail.com>
2018-03-22 16:16:25 +0100

    Authorization is now fully relying on listings

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -3,17 +3,23 @@ class BarcodesController < ApplicationController
   def show
     @ticket = Ticket.find(params[:ticket_id])
     @barcode = Barcode.find(params[:id])
+    @listing = ticket.listing_id
+    authorize @listing # only creator can view
   end
 
   def new
     @ticket = Ticket.find(params[:ticket_id])
     @barcode = Barcode.new
+    @listing = Listing.find(@ticket.listing_id)
+    authorize @listing
   end
 
   def create
     @ticket = Ticket.find(params[:ticket_id])
     @barcode = Barcode.new(barcode_params)
     @barcode.ticket = @ticket
+    @listing = Listing.find(@ticket.listing_id)
+    authorize @listing
     if @barcode.save
       redirect_to listing_ticket_path(@ticket.listing_id, @ticket)
       # We can still access the listing_id because this is stored inside @ticket
@@ -23,6 +29,8 @@ class BarcodesController < ApplicationController
   end
 
   def destroy
+    @listing = Listing.find(params[:listing_id])
+    authorize @listing
     @ticket = Ticket.find(params[:ticket_id])
     @barcode = Barcode.find(params[:id])
     @barcode.destroy
---
+++ app/policies/barcode_policy.rb
@@ -0,0 +1,7 @@
+class BarcodePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
