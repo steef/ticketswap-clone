4056b811ce407d92b0bf7ea05491e1897bf99bb5
Steef <stefanodegenkamp@gmail.com>
2018-03-22 11:16:30 +0100

    Has_many/belongs_to add to listing and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :listings
   has_many :tickets # only if the ticket is bought
 end
