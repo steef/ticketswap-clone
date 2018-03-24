a5b240fba724ea1b8791a25eeea5f05ac55661a3
Steef <stefanodegenkamp@gmail.com>
2018-03-24 16:55:18 +0100

    Validation 3 upgraded for second time with ticket placed for first time bugfix

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -5,13 +5,28 @@ class Barcode < ApplicationRecord
   validates :barcode, uniqueness: true, unless: :last_buyer
 
   def last_buyer
-    @barcode = Barcode.find_by barcode: barcode
-    unless @barcode.nil?
-      @ticket = Ticket.find_by id: ticket_id
-      @listing = Listing.find_by id: @ticket.listing_id
-      @barcode.ticket.user_id == @listing.user_id
+    @barcode_first = Barcode.where(barcode: barcode).first
+    @barcode_last = Barcode.where(barcode: barcode).last
+    # will look if barcode already exists
+    if @barcode_first.nil?
+      true
     else
-      false
+      if @barcode_first == @barcode_last
+        # checks if first/last update are same
+        @ticket = Ticket.find_by id: ticket_id
+        @listing = Listing.find_by id: @ticket.listing_id
+        @barcode_last.ticket.user_id == @listing.user_id
+        # checks if barcode seller id is same as last barcode buyer id
+        # same? responds with true
+      else
+        false
+      end
     end
   end
 end
+
+# After ticket is bought for the second time, it can't be sold again
+# Solution > Barcode.first / last check needs to be for specific user
+
+# Ticket can't be placed online for the first time
+# Solution > Needs to be bigger if else with if ticket isn't found, its true
