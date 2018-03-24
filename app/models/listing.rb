d7f059c93448c2f6436c7636b9f11017edb547c6
Steef <stefanodegenkamp@gmail.com>
2018-03-24 11:25:25 +0100

    Listing validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :tickets, dependent: :destroy
+
+  validates :selling_price, presence: true
+  validates :description, presence: true
 end
