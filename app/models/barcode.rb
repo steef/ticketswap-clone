0d807963ab541821e7d1d0301be9a2a70d5e4062
Steef <stefanodegenkamp@gmail.com>
2018-03-24 13:43:15 +0100

    Validation 3 build

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -2,4 +2,10 @@ class Barcode < ApplicationRecord
   belongs_to :ticket
 
   validates :barcode, presence: true
+  validates :barcode, uniqueness: true, unless: :last_buyer
+
+  def last_buyer
+    @barcode = Barcode.find_by barcode: barcode
+    @barcode.ticket.user_id == @barcode.ticket.listing
+  end
 end
