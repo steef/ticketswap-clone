97415dc50fb731f5ae54a205d4c8dda977393bbb
Steef <stefanodegenkamp@gmail.com>
2018-03-27 16:19:30 +0200

    User controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def profile
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+
 devise_for :users
   root to: 'listings#index'
 
