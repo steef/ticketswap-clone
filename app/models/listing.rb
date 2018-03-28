0dd18e59c1c49a6962ef4487eeea5154c6d37a21
Steef <stefanodegenkamp@gmail.com>
2018-03-28 11:42:02 +0200

    Validation for min price of 5 added

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :tickets, dependent: :destroy
 
-  validates :selling_price, presence: true
+  validates :selling_price, presence: true, numericality: { greater_than_or_equal_to: 5 }
   validates :description, presence: true
 end
